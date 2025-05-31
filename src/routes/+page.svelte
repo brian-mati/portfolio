ddf2f2eca807fbf54696c147930e90dd5e66ec1c
brian-mati <brianmuthomimati@gmail.com>
2025-05-31 23:55:33 +0300

    finished most content for first version on desktop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,32 @@
-<script></script>
+<header class="mb-[5%]">
+    <h1 class="text-base-headings font-bold text-headings">
+     Muthomi Brian Mati
+    </h1>
+    
+    <h2 class="text-base-primary text-subheading" > Software developer and web designer </h2>
+    
+    <p class="text-base-body tracking-wide leading-[120%] w-[900px] mx-auto">Hello there,  software developer is a vague way to state what I like to do. You can call me a web artisan.  I am most in my element problem solving my and my client's problems and developing efficient solutions to even the most complicated of problems. Simple I-right code that solves the issue on hand. In addition, i enjoy the creative process of coming up with a design for a project, though my perfectionist tendencies do get in the way at times....but the machine is strong.</p>
+</header>
 
-<h1 class="underline text-base-text">hello world</h1>
-<h1 class="underline text-base-text-body">hello world</h1>
+<main class="mb-[5%]">
+    <h2 class="text-base-headings text-subheading mb-2 " > Skills </h2>
+    <section class="grid grid-cols-3 grid-rows-2 gap-4 place-content-center w-[900px] mx-auto ">
+        <p class="text-base-body border-2 border-base-primary px-6 py-2 hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out"> User experience </p>
+        <p class="text-base-body border-2 border-base-primary px-6 py-2  hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out "> User Interfaces   </p>
+        <p class="text-base-body border-2 border-base-primary px-6 py-2 hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out "> JavaScript </p>
+        <p class="text-base-body border-2 border-base-primary px-6 py-2 hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out "> Html and css  </p>
+        <p class="text-base-body border-2 border-base-primary px-6 py-2 hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out "> SvelteKit</p>
+        <p class="text-base-body border-2 border-base-primary px-6 py-2 hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out "> Adonisjs</p>
+        <p class="text-base-body border-2 border-base-primary px-6 py-2 hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out  "> Gleam</p>
+    </section>
+</main>
+
+<footer>
+    <h2 class="text-base-headings text-subheading mb-2 " > Contact Info</h2>
+    <p class="text-base-body tracking-wide leading-[120%] w-[900px] mx-auto">Copy the email below . I will get back to you as soon as i can or call the number</p>
+    <section class="flex flex-col gap-4 justify-center items-center mt-4">
+        <p class="text-base-body border-2 border-base-primary px-6 py-2 hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out"> brianmuthomimati@gmail.com </p>
+        <p class="text-base-body border-2 border-base-primary px-6 py-2 hover:bg-base-primary/25 transition duration-150 delay-75 ease-in-out"> +254115151175 </p>
+    </section>
+    <p class="text-sm text-base-headings mt-[10%]">v.0.0.1</p>
+</footer>
