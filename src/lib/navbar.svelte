61b66306864fdf700e9558ee4af2f194d8149598
brian-mati <brianmuthomimati@gmail.com>
2025-06-13 14:32:59 +0300

    added view transitions

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<nav class="h-16 bg-gray-navs min-w-full border-2 border-b-black">
+    <ul class="flex items-center justify-between  gap-8 max-w-lg mx-auto py-4">
+        <a  aria-current={$page.url.pathname === '/' ? 'page' : undefined} href="/" class="hover:scale-125 transition duration-150 delay-75 hover:drop-shadow-sm hover:shadow-black">Home</a>
+
+        <a aria-current={$page.url.pathname === '/whoami' ? 'page' : undefined}  href="/whoami" class="hover:scale-125 transition duration-150 delay-75 hover:drop-shadow-sm hover:shadow-black">Whoami</a>
+
+        <a aria-current={$page.url.pathname === '/blog' ? 'page' : undefined}  href="/blog" class="hover:scale-125 transition duration-150 delay-75 hover:drop-shadow-sm hover:shadow-black">Blog</a>
+
+        <a aria-current={$page.url.pathname === '/contact-me' ? 'page' : undefined}  href="/contact-me" class="hover:scale-125 transition duration-150 delay-75 hover:drop-shadow-sm hover:shadow-black">Contact me</a>
+
+    </ul>
+</nav>
+
+<style>
+    a[aria-current='page']::before {
+	/* other existing rules */
+	view-transition-name: active-page;
+}
+</style>
---
+++ src/routes/whoami/+page.svelte
@@ -0,0 +1,3 @@
+
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, aliquam quod! A iusto nihil obcaecati delectus itaque dolore placeat voluptas, doloribus, perspiciatis corrupti reiciendis molestias praesentium nobis soluta saepe dolorum.</p>
