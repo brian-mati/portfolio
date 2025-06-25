0fa626746546d69b936113e1998ace9e49c132fd
brian-mati <brianmuthomimati@gmail.com>
2025-06-25 13:07:13 +0300

    feat:added authentication

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,19 @@
-import { auth } from '$lib/auth'; // path to your auth file
+import { auth } from '$lib/auth';
+import { redirect } from '@sveltejs/kit';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 export async function handle({ event, resolve }) {
+	const session = await auth.api.getSession({
+		headers: event.request.headers
+	});
+
+	if (session) {
+		event.locals.user = session.user;
+		event.locals.session = session.session;
+	// 	console.info(event.locals.user);
+	 }
+	if ( event.url.pathname.includes("dashboard") && !event.locals.session){
+		redirect(308,"/")
+	} 	
 	return svelteKitHandler({ event, resolve, auth });
 }
